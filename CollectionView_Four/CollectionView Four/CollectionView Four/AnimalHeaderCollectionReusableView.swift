d19f9912eb6cac21332aa414bedee589c007bab6
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2023-11-07 09:26:20 +0700

    Part 16 CollectionView from StepByStep Learning

---
+++ CollectionView_Four/CollectionView Four/CollectionView Four/AnimalHeaderCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalHeaderCollectionReusableView.swift
+//  CollectionView Four
+//
+//  Created by Bambang Tri Rahmat Doni on 07/11/23.
+//
+
+import UIKit
+
+final class AnimalHeaderCollectionReusableView: UICollectionReusableView {
+    @IBOutlet var titleLabel: UILabel!
+    
+}
---
+++ CollectionView_Four/CollectionView Four/CollectionView Four/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  CollectionView Four
+//
+//  Created by Bambang Tri Rahmat Doni on 07/11/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    @IBOutlet private var collectionView: UICollectionView!
+    
+    private var animals = [
+        "🐜", "🦇", "🦋", "🐈", "🐪", "🦀", "🦮", "🦅", "🐟",
+        "🦒", "🐐", "🦍", "🎠", "🦧", "🐁", "🦉", "🐇",
+        "🐀", "🦂", "🐅", "🐢", "🦓",
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() -> Void {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected collection at: \(indexPath.row)")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        switch kind {
+        case UICollectionView.elementKindSectionHeader:
+            if let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "AnimalHeaderCollectionReusableView", for: indexPath) as? AnimalHeaderCollectionReusableView {
+                headerView.titleLabel.text = "Header Label"
+                return headerView
+            }
+        case UICollectionView.elementKindSectionFooter:
+            if let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "AnimalFooterCollectionReusableView", for: indexPath) as? AnimalFooterCollectionReusableView {
+                footerView.titleLabel.text = "Footer Label"
+                return footerView
+            }
+        default:
+            return UICollectionReusableView()
+        }
+        
+        return UICollectionReusableView()
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnimalCell", for: indexPath) as? AnimalCollectionViewCell {
+            cell.animalEmojiLabel.text = animals[indexPath.row]
+            
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+}
