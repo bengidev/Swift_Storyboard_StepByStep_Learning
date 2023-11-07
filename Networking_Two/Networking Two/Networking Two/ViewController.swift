51b7032909a974d6f2cf3c34a7506bf72afdd70b
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2023-11-07 13:54:29 +0700

    Part 20 Networking from StepByStep Learning

---
+++ Networking_Two/Networking Two/Networking Two/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Networking One
+//
+//  Created by Bambang Tri Rahmat Doni on 07/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let baseURL = "https://jsonplaceholder.typicode.com"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        requestGETData(with: baseURL + "/posts")
+        requestPOSTData(with: baseURL + "/posts")
+    }
+
+    private func requestGETData(with urlString: String) -> Void {
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let responseData = data, let responseJson = try? JSONDecoder().decode([Post].self, from: responseData) {
+                print("URLSession data result: \(String(describing: data))")
+                print("URLSession response result: \(String(describing: responseJson))")
+                print("URLSession error result: \(String(describing: error))")
+            }
+        }
+        task.resume()
+    }
+    
+    private func requestPOSTData(with urlString: String) -> Void {
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Application/json", forHTTPHeaderField: "Content-Type")
+        
+        let post = Post(postId: 0, title: "POST Title", body: "POST Body", userId: 10)
+        if let httpBody = try? JSONEncoder().encode(post) {
+            request.httpBody = httpBody
+            print("POST \(httpBody)")
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let responseData = data, let responseJson = try? JSONDecoder().decode(Post.self, from: responseData) {
+                print("URLSession data result: \(String(describing: data))")
+                print("URLSession response result: \(String(describing: responseJson))")
+                print("URLSession error result: \(String(describing: error))")
+            }
+        }
+        task.resume()
+    }
+}
+
