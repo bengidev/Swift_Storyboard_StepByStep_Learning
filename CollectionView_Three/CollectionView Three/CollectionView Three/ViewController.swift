77648ada4d95deb11ca71e18aafb05d52163eecd
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2023-11-07 08:40:52 +0700

    Part 15 CollectionView from StepByStep Learning

---
+++ CollectionView_Three/CollectionView Three/CollectionView Three/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  CollectionView Three
+//
+//  Created by Bambang Tri Rahmat Doni on 07/11/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    @IBOutlet private var collectionView: UICollectionView!
+    
+    private var animals = [
+        "🐜", "🦇", "🦋", "🐈", "🐪", "🦀", "🦮", "🦅", "🐟",
+        "🦒", "🐐", "🦍", "🎠", "🦧", "🐁", "🦉", "🐇",
+        "🐀", "🦂", "🐅", "🐢", "🦓",
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() -> Void {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(UINib(nibName: "AnimalCollectionViewCell", bundle:
+                                        Bundle(for: AnimalCollectionViewCell.self)),
+                                forCellWithReuseIdentifier: "AnimalCollectionViewCell")
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected collection at: \(indexPath.row)")
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnimalCollectionViewCell", for: indexPath) as? AnimalCollectionViewCell {
+            cell.animalEmojiLabel.text = animals[indexPath.row]
+            
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+}
