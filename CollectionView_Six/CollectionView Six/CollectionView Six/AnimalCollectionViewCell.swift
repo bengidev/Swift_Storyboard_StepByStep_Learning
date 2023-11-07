ec364410eaf8b03c7cce675475ceabcf3f9c066b
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2023-11-07 10:54:54 +0700

    Part 18 CollectionView from StepByStep Learning

---
+++ CollectionView_Six/CollectionView Six/CollectionView Six/AnimalCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AnimalCollectionViewCell.swift
+//  CollectionView Six
+//
+//  Created by Bambang Tri Rahmat Doni on 07/11/23.
+//
+
+import UIKit
+
+final class AnimalCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var animalEmojiLabel: UILabel!
+}
