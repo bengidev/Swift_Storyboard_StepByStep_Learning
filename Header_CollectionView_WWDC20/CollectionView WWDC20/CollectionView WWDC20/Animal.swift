26b4842e99625be4d26b03ea7385c375830c2e5c
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2023-11-14 05:38:00 +0700

    Part 43 Header CollectionView WWDC from StepByStep

---
+++ Header_CollectionView_WWDC20/CollectionView WWDC20/CollectionView WWDC20/Animal.swift
@@ -0,0 +1,13 @@
+//
+//  Animal.swift
+//  CollectionView WWDC20
+//
+//  Created by Bambang Tri Rahmat Doni on 12/11/23.
+//
+
+import Foundation
+
+struct Animal: Hashable {
+    let image: String
+    let name: String
+}
